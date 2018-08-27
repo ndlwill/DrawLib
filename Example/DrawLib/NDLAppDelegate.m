992e1bffe4acc5f324de6eb91933117d3ff974e9
dzcx-ndl <ndlwill1020@126.com>
2018-08-27 16:32:33 +0800

    Revert "Revert "1""
    This reverts commit e98cf47b32f3fada35f0d562983a5a7552ee34eb.

--- Example/DrawLib/NDLAppDelegate.m
+++ Example/DrawLib/NDLAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    // 123456
     return YES;
 }
 
